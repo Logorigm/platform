3149ad842839864751faedeb282dfc6c11aa3243
Logorigm <miketransformer16@gmail.com>
2025-05-18 10:38:44 +0600

    Present commit

--- core/src/main/java/com/logorigm/Duck.java
+++ core/src/main/java/com/logorigm/Duck.java
@@ -12,14 +12,16 @@ import com.badlogic.gdx.math.Rectangle;
 public class Duck {
 
     public Rectangle duckRectangle = new Rectangle(38, 65, 32, 32);
-    private final Texture duckTexture = new Texture("Dug.png");
+    private Texture duckTexture = new Texture("Dug.png");
 
     private float dash_time = 0;
     private float jump_time = 0;
     private float jump_block = 0;
+    private int ch = 0;
 
 
     public void input(TiledMap map) {
+        spikes(map);
         float dspeed = 100f;
         float delta = Gdx.graphics.getDeltaTime();
         float speed = 200f;
@@ -52,7 +54,6 @@ public class Duck {
                 duckRectangle.y+=0.5f;
             }
         }
-
         var ren = duckRectangle.getY();
         if (Gdx.input.isKeyPressed(Input.Keys.D) && r!=0) {
             duckRectangle.x += (speed * delta);
@@ -61,12 +62,22 @@ public class Duck {
             duckRectangle.x += (-speed * delta);
         }
         if (Gdx.input.isKeyPressed(Input.Keys.W) && h!=0 && d==0) {
-            jump_time += delta;
-            duckRectangle.y += 10f;
+            duckRectangle.y += 100f;
         }
-        else if (ren>0 && d!=0) {
+        else if (ren>0 && d!=0 && ch!=1) {
             duckRectangle.y+=(-dspeed * delta);
         }
+        /*if(ch == 1 && jump_time < 2){
+            duckRectangle.y += 10f;
+            jump_time += delta;
+        }
+        if(jump_time >= 2){
+            ch = 2;
+            jump_time -= delta;
+        }
+        if(ch == 2 && jump_time <= 0){
+            ch = 0;
+        }*/
         if (Gdx.input.isKeyPressed(Input.Keys.E) && dash_time <= 0) {
             duckRectangle.x += 50;
             dash_time += 50*delta;
@@ -83,5 +94,39 @@ public class Duck {
 
 
         duckRectangle.set(duckRectangle.getX(), duckRectangle.getY(), duckWidth, duckHeight);
+    }
+    public void spikes(TiledMap map){
+        int ls=0,rs=0,hs=0,ds=0;
+        TiledMapTileLayer layer = (TiledMapTileLayer)map.getLayers().get("Spikes");
+        for(int i=0;i<4;i++) {
+            TiledMapTileLayer.Cell cell = layer.getCell((int)(duckRectangle.x+20)/32, (int)(duckRectangle.y-20)/32);
+            if(cell == null){
+                ds = 1;
+            }
+            cell = layer.getCell((int)(duckRectangle.x)/32, (int)(duckRectangle.y)/32);
+            if(cell == null){
+                ls = 1;
+            }
+            cell = layer.getCell((int)(duckRectangle.x)/32, (int)(duckRectangle.y)/32);
+            if(cell == null){
+                rs = 1;
+            }
+            else{
+                duckRectangle.x-=0.5f;
+            }
+            cell = layer.getCell((int)(duckRectangle.x)/32, (int)(duckRectangle.y)/32);
+            if(cell == null){
+                hs = 1;
+            }
+            else{
+                duckRectangle.y+=0.5f;
+            }
+        }
+        if(hs == 0 || rs == 0 || ls == 0 || ds ==0){
+            //duckTexture = new Texture("DeadDuck.png");
+        }
+        else{
+            duckTexture = new Texture("Dug.png");
+        }
     }
 }
--- core/src/main/java/com/logorigm/Main.java
+++ core/src/main/java/com/logorigm/Main.java
@@ -77,7 +77,6 @@ public class Main extends ApplicationAdapter {
         input();
         logic();
         draw();
-
     }
     private void logic() {
 
